20e00c301ed9aa55c2b852a444e44062be35b013
HenriUwU <henrisebille69@gmail.com>
2026-03-16 15:04:56 +0100

    feat: starting logreg_train.py

---
+++ Part III/logreg_train.py
@@ -0,0 +1,30 @@
+import argparse
+
+import pandas as pd
+from colorama import Fore, Style
+
+
+def main(path: str) -> None:
+    try:
+        df = pd.read_csv(path)
+
+    except Exception as e:
+        print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+
+
+if __name__ == "__main__":
+    # Create the argument parser
+    parser = argparse.ArgumentParser(
+        description=""
+    )
+
+    # Mandatory CSV file argument
+    parser.add_argument('path',
+                        type=str,
+                        help='path to file (the dataset)')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call the main function
+    main(args.path)
