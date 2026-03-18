82ec65f556d33f5bfa1d4c20c9d86cd038f1fe07
Rart3x <ramjattankeny971@gmail.com>
2026-03-18 13:55:08 +0100

    feat: pair-plot is clickable, can open each subplot to see more precisely informations

--- Part II/pair_plot.py
+++ Part II/pair_plot.py
@@ -74,7 +74,53 @@ def main(path: str) -> None:
 
         plt.subplots_adjust(wspace=0.1, hspace=0.1, bottom=0.15, left=0.15)
 
+        def on_click(event):
+            if event.inaxes is None or event.dblclick is False:
+                return
+
+            clicked_ax = event.inaxes
+
+            try:
+                idx = list(axes).index(clicked_ax)
+            except ValueError:
+                return
+
+            row_idx = idx // nb_rows_lines
+            col_idx = idx % nb_rows_lines
+
+            subject_line = subjects[row_idx]
+            subject_column = subjects[col_idx]
+
+            fig_new, ax_new = plt.subplots(figsize=(6, 6))
+
+            if subject_column == subject_line:
+                for house in houses:
+                    house_idx = df["Hogwarts House"] == house
+                    subject_score = df[house_idx][subject_line]
+                    clean_subject_score = subject_score.dropna()
+                    ax_new.hist(clean_subject_score, label=house, alpha=0.5)
+            else:
+                for house in houses:
+                    df_house = df[df["Hogwarts House"] == house].copy()
+                    clean_df_house = df_house.dropna()
+                    ax_new.scatter(
+                        x=clean_df_house[subject_line],
+                        y=clean_df_house[subject_column],
+                        alpha=0.5,
+                        label=house,
+                        marker=".",
+                        s=15
+                    )
+
+            ax_new.set_xlabel(subject_column)
+            ax_new.set_ylabel(subject_line)
+            ax_new.legend()
+            plt.show()
+
+        fig.canvas.mpl_connect('button_press_event', on_click)
+
         plt.show()
+
     except Exception as e:
         print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
 
