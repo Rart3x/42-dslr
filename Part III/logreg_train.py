3d07762777ac0df0c41d4a729f2c92dc5411029e
HenriUwU <henrisebille69@gmail.com>
2026-03-19 14:39:05 +0100

    feat: bonus - adding learning curve and early stop for each model

--- Part III/logreg_train.py
+++ Part III/logreg_train.py
@@ -1,10 +1,12 @@
 import argparse
+from typing import Tuple, List
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from colorama import Fore, Style
 
-nb_epochs = 10000
+nb_epochs = 50000
 learning_rate = 0.01
 
 features_to_keep = [
@@ -77,7 +79,30 @@ def sigmoid(z: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-z))
 
 
-def train(X: np.ndarray, y: np.ndarray) -> np.ndarray:
+def compute_loss(y: np.ndarray, h: np.ndarray) -> float:
+    """
+    Calculates the loss using the log-loss (binary cross entropy) function.
+
+    y : the vector containing the answers to the prediction
+    h : the vector containing the predictions done by our model
+
+    :return: the loss
+    """
+
+    # Iterate through the predictions
+    # replaces the value of h by eps if it smaller than eps
+    # replaces the value of h by 1 - eps if it is higher than eps
+    # This prevents abnormal calculations if the prediction is 1 or 0
+    eps = 1e-15
+    h = np.clip(h, eps, 1 - eps)
+
+    loss = (- (1 / y.shape[0]) *
+            np.sum((y * np.log(h) + (1 - y) * np.log(1 - h))))
+
+    return loss
+
+
+def train(X: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, List[float]]:
     """
     The function that trains our model through the gradient descent.
 
@@ -88,31 +113,60 @@ def train(X: np.ndarray, y: np.ndarray) -> np.ndarray:
 
     :param X: matrix of data.
     :param y: vector containing the answers
-    :return: the weights after the training
+    :return: the weights after the training, the loss history
     """
     W = np.zeros(X.shape[1])
+    loss_history = []
+
+    # Tolerance to know if our model is still learning
+    tolerance = 1e-7
 
     for epoch in range(nb_epochs):
         Z = np.dot(X, W)
         predictions = sigmoid(Z)
+        loss_history.append(compute_loss(y, predictions))
+
+        # Early stopping, if our learning is lower than the tolerance, we stop
+        if epoch > 1:
+            learning = abs(loss_history[epoch] - loss_history[epoch - 1])
+            if learning < tolerance:
+                break
+
         error = predictions - y
         gradient = 1 / X.shape[0] * np.dot(np.transpose(X), error)
         W = W - (learning_rate * gradient)
 
-    return W
+    return W, loss_history
 
 
 def main(path: str) -> None:
     try:
         df = pd.read_csv(path)
-
         X = process_data(df)
-        weights_for_house: dict[str, np.ndarray] = {}
 
+        weights_for_house: dict[str, np.ndarray] = {}
+        training_loss_history: dict[str, List[float]] = {}
+
+        # Train our four models, one per house
         for house in houses:
             y = (df["Hogwarts House"] == house).astype(int).to_numpy()
-            weights_for_house[house] = train(X, y)
+            (weights_for_house[house],
+             training_loss_history[house]) = train(X, y)
 
+        fig, axes = plt.subplots(nrows=2, ncols=2)
+        axes = axes.flatten()
+
+        # Plot the learning curve of each model, one per house
+        for i, house in enumerate(houses):
+            axes[i].plot(training_loss_history[house])
+            axes[i].set_title(house)
+            axes[i].set_xlabel("Epochs")
+            axes[i].set_ylabel("Loss")
+
+        plt.tight_layout()
+        plt.show()
+
+        # Save the weights in a file
         np.savez("weights.npz", **weights_for_house)
 
         print(f"{Fore.GREEN}"
